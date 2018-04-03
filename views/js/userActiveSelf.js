abef3d1ca51f57f1f43de95fd981108dea8a3fbe
HengliYang <1029884642@qq.com>
2018-04-03 11:34:28 +0800

    hha

--- views/js/userActiveSelf.js
+++ views/js/userActiveSelf.js
@@ -4,7 +4,7 @@ $(function () {
             render: "canvas", //也可以替换为table
             width: 100,
             height: 100,
-            text: "http://www.baidu.com?identitycode=" + getIdentifycode()/*可以通过ajax请求动态设置*/
+            text: "https://mobipromo.io/telegram?identitycode=" + getIdentifycode()/*可以通过ajax请求动态设置*/
         }).hide();
         //将生成的二维码转换成图片格式
         var canvas = qrcode.find('canvas').get(0);
